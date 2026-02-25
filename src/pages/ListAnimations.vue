952e1554f2e046a7625b8d1fe930182aac984b72
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-25 09:58:21 -0500

    list animations

--- src/pages/ListAnimations.vue
+++ src/pages/ListAnimations.vue
@@ -2,5 +2,16 @@
   <div class="container">
     <h2>List Animations</h2>
     <router-link to="/other">Other Animations</router-link>
+    <ListData />
   </div>
 </template>
+
+<script>
+import ListData from "@/components/ListData.vue";
+
+export default {
+  components: {
+    ListData,
+  },
+};
+</script>
