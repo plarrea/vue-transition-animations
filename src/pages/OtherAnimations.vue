15763f0ed2e3997396a846f4ba319f51950618fa
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-25 11:39:20 -0500

    other animations examples

--- src/pages/OtherAnimations.vue
+++ src/pages/OtherAnimations.vue
@@ -2,5 +2,179 @@
   <div class="container">
     <h2>Other Animations</h2>
     <router-link to="/">List Animations</router-link>
+    <div class="container">
+      <div class="block" :class="{ animate: animatedBlock }"></div>
+      <button @click="animateBlock">Animate</button>
+    </div>
+
+    <div class="container">
+      <transition>
+        <p v-if="paragraphIsVisible">This is only visible sometimes...</p>
+      </transition>
+      <transition
+        :css="false"
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @before-leave="beforeLeave"
+        @leave="leave"
+        @enter-cancelled="enterCancelled"
+        @leave-cancelled="leaveCancelled"
+      >
+        <p v-if="paragraphIsVisible">And this one is controlled by code</p>
+      </transition>
+      <button @click="toggleParagraph">Toggle Paragraph</button>
+    </div>
+
+    <div class="container">
+      <transition name="fade-button" mode="out-in">
+        <button v-if="usersAreVisible" @click="hideUsers">Hide Users</button>
+        <button v-else @click="showUsers">Show Users</button>
+      </transition>
+    </div>
+
+    <base-modal @close="hideDialog" :open="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
+    <div class="container">
+      <button @click="showDialog">Show Dialog</button>
+    </div>
   </div>
 </template>
+
+<script>
+let enterInterval;
+let leaveInterval;
+
+export default {
+  data() {
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paragraphIsVisible: false,
+      usersAreVisible: false,
+    };
+  },
+  methods: {
+    enterCancelled() {
+      clearInterval(enterInterval);
+    },
+    leaveCancelled() {
+      clearInterval(leaveInterval);
+    },
+    beforeEnter(el) {
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      let round = 1;
+      enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    beforeLeave(el) {
+      el.style.opacity = 1;
+    },
+    leave(el, done) {
+      let round = 1;
+      leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    toggleParagraph() {
+      this.paragraphIsVisible = !this.paragraphIsVisible;
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform 0.25s ease-out; */
+}
+
+.animate {
+  /* transform: translateX(-150px); */
+  animation: slide-scale 0.3s ease-out forwards;
+}
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.v-enter-active {
+  transition: all 0.3s ease-out;
+  /* animation: slide-scale 0.3s ease-out; */
+}
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.v-leave-active {
+  transition: all 0.3s ease-in;
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+</style>
